0b4802258a1afb6894490beaf55a07e1e187fb8b
Yveskab <kabobayves@gmail.com>
2020-07-08 20:50:27 +0200

    display map

--- app/src/main/java/yves/mycompany/alertsystem/MainActivity.java
+++ app/src/main/java/yves/mycompany/alertsystem/MainActivity.java
@@ -2,10 +2,15 @@ package yves.mycompany.alertsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void requestForSomeone(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
